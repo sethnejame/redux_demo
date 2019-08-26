b6da5576b15ff3cf921e9059fd2a7023ef2da50e
Seth NeJame <sethnejame@gmail.com>
2019-08-26 07:12:42 +0200

    Bringing PostForm into App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
-import Posts from './components/Posts';
+import React, { Component } from "react";
+import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
 
-function App() {
-  return (
-    <div className="App">
-      <Posts />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    );
+  }
 }
 
 export default App;
