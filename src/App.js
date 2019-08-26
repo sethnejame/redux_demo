b5cf6c04298eb1bc213a64612057fe75d98ba371
Seth NeJame <sethnejame@gmail.com>
2019-08-26 07:35:21 +0200

    Wrapping App component in provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
+import { Provider } from "react-redux";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
+      <Provider>
+        <div className="App">
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
