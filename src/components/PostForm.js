9df0854ba9286a0ca134618d66cb3cfee036c83f
Seth NeJame <sethnejame@gmail.com>
2019-08-26 07:15:54 +0200

    Adding form input to PostForm

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -10,6 +10,21 @@ class PostForm extends Component {
     return (
       <div>
         <h1>Add Post</h1>
+        <form>
+          <div>
+            <label>Title: </label>
+            <br />
+            <input type="text" name="title" />
+          </div>
+          <br />
+          <div>
+            <label>Body: </label>
+            <br />
+            <textarea name="body" />
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
