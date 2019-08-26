e15a680a60d4cbe44abb46f40eee869947da0717
Seth NeJame <sethnejame@gmail.com>
2019-08-26 07:43:38 +0200

    Creating store in App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
 import { Provider } from "react-redux";
+import { applyMiddleware } from "redux";
+import { createStore } from "redux";
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
     return (
-      <Provider>
+      <Provider store={store}>
         <div className="App">
           <PostForm />
           <hr />
